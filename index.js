7fc1384ddc3808147bc099a0b2971c45e7bdefd0
Ellyn <eka249@nyu.edu>
2019-08-26 22:01:10 -0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+import { write } from "fs";
+
+function writeCards(namesArray, event) {
+    let final=[]
+    for (let i=0; i < namesArray.length; i++) {
+    final.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return final
+};
+
+
+
+
+let x= 4;
+
+function countdown(x){
+    let i=x;
+    while (i>=0){
+        console.log(i)
+        i--;
+    }
+}
+
+
+
+
