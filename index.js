b4498b9920f541e4147680a151645680bd0256e1
marshallhelenm <marshallhelenm@gmail.com>
2019-08-27 11:32:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(array, event) {
+    let newarr = []
+    for(let i = 0; i < array.length; i++) {    
+        newarr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return newarr
+
+}
+
+function countdown(num){
+    for(let i = 0; i < num + 1; i++){
+        console.log(i)
+    }
+}
